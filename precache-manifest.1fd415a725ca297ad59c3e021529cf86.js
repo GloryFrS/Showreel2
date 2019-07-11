91e2d2b0e1fa6bbb17a1527cbff532034115d844
gloryfrs <gloryfrs@yandex.ru>
2019-07-11 12:40:24 +0700

    Updates

---
+++ precache-manifest.1fd415a725ca297ad59c3e021529cf86.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "825f43f4d4cd8e82c7e75307e9b7ba96",
+    "url": "/Showreel2/static/media/Spot_HI.825f43f4.png"
+  },
+  {
+    "revision": "f15286de8ebf05ea1775",
+    "url": "/Showreel2/static/css/main.9c636ed1.chunk.css"
+  },
+  {
+    "revision": "c27ba78528840175d5bb",
+    "url": "/Showreel2/static/js/runtime~main.c27ba785.js"
+  },
+  {
+    "revision": "2dddc22519122dc39a66aa80198afe65",
+    "url": "/Showreel2/static/media/Spot_interest.2dddc225.png"
+  },
+  {
+    "revision": "909cce95b5357f1a3177",
+    "url": "/Showreel2/static/js/2.909cce95.chunk.js"
+  },
+  {
+    "revision": "897bcbfd42520d4e47129df9a5d9219b",
+    "url": "/Showreel2/static/media/Spot_relax.897bcbfd.png"
+  },
+  {
+    "revision": "f15286de8ebf05ea1775",
+    "url": "/Showreel2/static/js/main.f15286de.chunk.js"
+  },
+  {
+    "revision": "b1e4c530665d0447cc04f18c6860739c",
+    "url": "/Showreel2/static/media/Push_example.b1e4c530.svg"
+  },
+  {
+    "revision": "da9a12d62f16785945b55b4c59a4f003",
+    "url": "/Showreel2/static/media/Spot_mobile.da9a12d6.png"
+  },
+  {
+    "revision": "e2ccc3e2711367dd36593c1b350b8d10",
+    "url": "/Showreel2/static/media/Persik_hungry.e2ccc3e2.png"
+  },
+  {
+    "revision": "f449cff638623fc8819dac413b4cfc8d",
+    "url": "/Showreel2/static/media/Persik_glad.f449cff6.png"
+  },
+  {
+    "revision": "909cce95b5357f1a3177",
+    "url": "/Showreel2/static/css/2.e72fa513.chunk.css"
+  },
+  {
+    "revision": "8593beb1e77bcc0393752d3568480927",
+    "url": "/Showreel2/index.html"
+  }
+];
